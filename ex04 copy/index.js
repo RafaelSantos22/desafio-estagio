3c13ca1fefb470002ddac4a5c81bb5b86177ce9f
Rafael Santos <rafa_santos33@outlook.com>
2024-03-11 13:27:59 -0300

    Exercício 05 finalizado

---
+++ ex04 copy/index.js
@@ -0,0 +1,12 @@
+const inverterString = (string) => {
+    let stringInvertida = '';
+
+    for (let i = string.length - 1; i >= 0; i--) {
+       stringInvertida += string[i];
+    }
+
+    return stringInvertida;
+}
+
+console.log(inverterString('carro'));
+// Saída = 'orrac'
